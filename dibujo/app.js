8cf32df9f86619ac0c7432ff730582c4ecf38f8e
yaretzi.ortiz <ortizyarezti@gmail.com>
2024-10-15 13:08:55 -0600

    05 Dibujo Canvas

---
+++ dibujo/app.js
@@ -0,0 +1,207 @@
+const lienzo = document.getElementById('lienzo');
+const ctx = lienzo.getContext('2d');
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(200, 100, 75, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(180, 120, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(275, 120, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(295, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(315, 120, 20, 20);
+ctx.fill();
+///
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(335, 100, 75, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(410, 120, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(160, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(160, 160, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(160, 180, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(180, 200, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(200, 220, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(220, 240, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(240, 260, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(260, 280, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(280, 300, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(300, 300, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(310, 300, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(330, 280, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(350, 260, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(370, 240, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(390, 220, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(410, 200, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(430, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(430, 160, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(430, 180, 20, 20);
+ctx.fill();
+
+//----------------------------------------------- ROJO
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(200, 120, 75, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(180, 140, 115, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(335, 120, 75, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(315, 140, 115, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(180, 160, 250, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(180, 180, 250, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(200, 200, 210, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(220, 220, 170, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(240, 240, 130, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(260, 260, 90, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(280, 280, 50, 20);
+ctx.fill();
+
+
+//------------- BRILLO
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(200, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(220, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(200, 160, 20, 20);
+ctx.fill();
+
+
---
+++ hellok/app.js
@@ -0,0 +1,418 @@
+const lienzo = document.getElementById('lienzo');
+const ctx = lienzo.getContext('2d');
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(160, 60, 80, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(160, 80, 20, 60);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(240, 80, 120, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(320, 100, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(320, 120, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(360, 60, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(420, 60, 80, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(480, 80, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(480, 100, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(480, 120, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(180, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(160, 160, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(140, 180, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(140, 200, 20, 20);
+ctx.fill();
+// ------------------bigotes
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(120, 200, 60, 20);
+ctx.fill();
+
+// ------------------------------
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(140, 220, 20, 20);
+ctx.fill();
+
+// --------------bigotes
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(120, 240, 60, 20);
+ctx.fill();
+// -----------------------
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(160, 260, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(160, 280, 20, 20);
+ctx.fill();
+
+// ---------- patita 1
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(180, 280, 60, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(200, 280, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(180, 300, 20, 40);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(200, 340, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(240, 300, 20, 20);
+ctx.fill();
+// ---------------------------
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(240, 320, 160, 20);
+ctx.fill();
+
+// ----------- patita 2
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(380, 300, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(400, 340, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(440, 300, 20, 40);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(400, 280, 80, 20);
+ctx.fill();
+
+// ----------
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(460, 240, 20, 60);
+ctx.fill();
+
+// ------------ bigote 
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(460, 260, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(480, 220, 40, 20);
+ctx.fill();
+
+// ----------------
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(440, 200, 60, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(480, 180, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(500, 140, 20, 40);
+ctx.fill();
+
+//--------------- MOÑO
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(460, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(440, 120, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(420, 100, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(400, 80, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(380, 100, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(360, 120, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(340, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(380, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(400, 160, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(420, 180, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(440, 160, 20, 20);
+ctx.fill();
+
+// ---------COLOR DE RELLENO DEL MOÑO
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(360, 80, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(340, 100, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(340, 120, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(400, 100, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(380, 120, 60, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(400, 140, 60, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(420, 160, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(480, 140, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(460, 160, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'red';
+ctx.roundRect(440, 180, 40, 20);
+ctx.fill();
+
+// --------relleno HELLO KITTY
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(180, 80, 60, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(420, 80, 60, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(180, 100, 140, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(440, 100, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(180, 120, 140, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(460, 120, 20, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(200, 140, 140, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(360, 140, 20, 20);
+ctx.fill()
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(180, 160, 220, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(160, 180, 260, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(180, 200, 260, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(160, 220, 320, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(180, 240, 280, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(180, 260, 280, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(240, 280, 160, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(260, 300, 120, 20);
+ctx.fill();
+
+//----- RELLENO DE PATITAS
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(200, 300, 40, 40);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'white';
+ctx.roundRect(400, 300, 40, 40);
+ctx.fill();
+
+// -------- OJOS
+// Izquierdo
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(380, 200, 40, 40);
+ctx.fill();
+
+// Derecho
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.roundRect(220, 200, 40, 40);
+ctx.fill();
+
+// ----------NARIZ
+ctx.beginPath();
+ctx.fillStyle = '#e2cf13';
+ctx.roundRect(300, 240, 40, 20);
+ctx.fill();
+
+// ----------MEJILLAS
+ctx.beginPath();
+ctx.fillStyle = '#f390c3';
+ctx.roundRect(200, 240, 40, 20);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = '#f390c3';
+ctx.roundRect(400, 240, 40, 20);
+ctx.fill();
